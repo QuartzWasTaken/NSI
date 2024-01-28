8ece8ef135469cd7e38f0e7467356fd92f9ade14
QuartzWasTaken <QuartzWasTaken@users.noreply.github.com>
2024-01-28 23:35:26 +0100

    Classe Log basique, code pourri

--- Projets faits maison/CPPBasics/CPPBasics/Log.cpp
+++ Projets faits maison/CPPBasics/CPPBasics/Log.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
 
-void Log(const char* output)
-{
-	std::cout << output << std::endl;
-}
-
 int Ask()
 {
 	int input;
--- Projets faits maison/CPPBasics/CPPBasics/main.cpp
+++ Projets faits maison/CPPBasics/CPPBasics/main.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
 
-void Log(const char*);
 int Ask();
+
+class Log
+{
+public:
+	int LogLevelError = 2;
+	int LogLevelWarning = 1;
+	int LogLevelInfo = 0;
+
+private:
+	int m_LogLevel = 0;
+
+public:
+	void setLevel(int level)
+	{
+		m_LogLevel = level;
+	}
+
+
+	void info(const char* message)
+	{
+		if(m_LogLevel <= LogLevelInfo)
+			std::cout << "[Information] : " << message << std::endl;
+	}
+	void error(const char* message)
+	{
+		if (m_LogLevel <= LogLevelError)
+			std::cout << "[Error] : " << message << std::endl;
+	}
+	void warn(const char* message)
+	{
+		if (m_LogLevel <= LogLevelWarning)
+			std::cout << "[Warning] : " << message << std::endl;
+	}
+};
+
 
 int main()
 {
-	char* ptr = new char[8];
-	memset(ptr, 0, 8);
-	std::cout << *ptr;
-
-	delete[8] ptr;
-	
+	Log log;
+	log.setLevel(log.LogLevelWarning);
+	log.error("This is an error...!?");
+	log.warn("This is a warning...!?");
+	log.info("This is an info...!?");
 }
