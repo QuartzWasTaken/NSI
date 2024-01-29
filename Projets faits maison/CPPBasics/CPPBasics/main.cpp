ba326e587af0bc524709b7e65b1c6012ed09e827
QuartzWasTaken <QuartzWasTaken@users.noreply.github.com>
2024-01-29 23:44:05 +0100

    Ajout variables static + constructeurs

--- Projets faits maison/CPPBasics/CPPBasics/main.cpp
+++ Projets faits maison/CPPBasics/CPPBasics/main.cpp
@@ -5,12 +5,11 @@ int Ask();
 class Log
 {
 public:
-	int LogLevelError = 2;
-	int LogLevelWarning = 1;
-	int LogLevelInfo = 0;
-
-private:
-	int m_LogLevel = 0;
+	enum Level
+	{
+		Info, Warn, Error
+	};
+	static int m_LogLevel;
 
 public:
 	void setLevel(int level)
@@ -18,30 +17,47 @@ public:
 		m_LogLevel = level;
 	}
 
+	Log()
+	{
+		m_LogLevel = 0;
+	}
 
 	void info(const char* message)
 	{
-		if(m_LogLevel <= LogLevelInfo)
+		if(m_LogLevel <= Info)
 			std::cout << "[Information] : " << message << std::endl;
 	}
 	void error(const char* message)
 	{
-		if (m_LogLevel <= LogLevelError)
+		if (m_LogLevel <= Error)
 			std::cout << "[Error] : " << message << std::endl;
 	}
 	void warn(const char* message)
 	{
-		if (m_LogLevel <= LogLevelWarning)
+		if (m_LogLevel <= Warn)
 			std::cout << "[Warning] : " << message << std::endl;
 	}
+
+
+
 };
 
+int Log::m_LogLevel;
 
 int main()
 {
 	Log log;
-	log.setLevel(log.LogLevelWarning);
+	Log log2;
+
+	log.setLevel(log.Warn);
+
 	log.error("This is an error...!?");
 	log.warn("This is a warning...!?");
 	log.info("This is an info...!?");
+
+	log.setLevel(log.Info);
+
+	log2.error("This is an error...!?");
+	log2.warn("This is a warning...!?");
+	log2.info("This is an info...!?");
 }
